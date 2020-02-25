9b2e3a8605cf5a3b6ac3ef06c82017d378e98ac0
Mantvydas <civilka.mantas@gmail.com>
2020-02-25 20:57:03 +0200

    Implementing dropdown. WIP #2

--- src/script.js
+++ src/script.js
@@ -35,8 +35,9 @@ class SearchApi {
 
 let searchService = new SearchApi()
 
-inputElement.onkeydown = function () {
-    dropdown.innerHTML=""
+inputElement.addEventListener("keydown", function () {
+
+    dropdown.innerHTML = ""
     const value = this.value;
     if (value.length >= 3) {
         searchService.searchMovies(value).then(movies => {
@@ -44,6 +45,11 @@ inputElement.onkeydown = function () {
             movieList.splice(0, 8).forEach(movie => {
                 const option = movieOption(movie)
                 dropdown.appendChild(option)
+                let x = document.querySelectorAll(".title")
+                // console.log(x)
+                x.forEach(element => element.addEventListener("click", (e) => {
+                    inputElement.value = e.target.textContent
+                }))
             })
 
         })
@@ -51,24 +57,41 @@ inputElement.onkeydown = function () {
     else {
         dropdown.innerHTML = `<p>this was deleted</p>`
     }
-}
+})
 
-// window.addEventListener("click", (e) => {
-//     if(!event.target.matches("#dropdown")) {
-//     dropdown.classList.remove("show")
-//     }
+window.addEventListener("click", (e) => {
+    if (!e.target.matches(".dropdown-item")) {
+        dropdown.classList.remove("show")
+    }
+    
+})
 
-// })
+window.addEventListener("click", (e) => {
+    console.log(e.target)
+    if (e.target.matches(".searchTerm"))
+        dropdown.classList.add("show")
+})
+
 
 function movieOption(movie) {
     let content = document.createElement("div")
+    content.classList.add("dropdown-item")
     let movieName = document.createElement("div")
     movieName.classList.add("title")
     movieNameContent = document.createTextNode(`${movie.title}`)
     movieName.appendChild(movieNameContent)
     content.appendChild(movieName)
+
     let rating = document.createElement("div")
     rating.classList.add("rating")
+    ratingContent = document.createTextNode(`${movie.vote_average}`)
+    rating.appendChild(ratingContent)
     content.appendChild(rating)
+
+    let movieYear = document.createElement("div")
+    movieYear.classList.add("year")
+    movieYearContent = document.createTextNode(`${movie.release_date.slice(0, 4)}`)
+    movieYear.appendChild(movieYearContent)
+    content.appendChild(movieYear)
     return content
 }
