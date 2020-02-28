bfe8cd9555171ee99e3e1e223a89fb9168d1678f
Mantvydas <civilka.mantas@gmail.com>
2020-02-28 19:10:25 +0200

    Improve dropdown style and logic

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 const inputElement = document.querySelector(".searchTerm");
 const buttonElement = document.querySelector(".searchButton");
-const dropdown = document.getElementById("dropdown")
-
+const dropdown = document.getElementById("dropdown");
+const textholder = document.querySelector(".textholder")
 function sortByScore(a, b) {
     return b.popularity - a.popularity
 }
@@ -12,8 +12,6 @@ class SearchApi {
         this.url = "https://api.themoviedb.org/3/search/movie";
 
     }
-
-
 
     searchMovies(queryString) {
         let url = this._createUrl(queryString)
@@ -28,70 +26,75 @@ class SearchApi {
     _createUrl(queryString) {
         const sanitizedQueryString = queryString.replace(/ |&/g, function (match) { return (match == "&") ? "%26" : "%20"; })
         const url = `${this.url}?api_key=${this.apiKey}&language=en-US&query=${sanitizedQueryString}&page=1&include_adult=false`
-        return url
+        return url;
     }
-
 }
 
 let searchService = new SearchApi()
 
 inputElement.addEventListener("keydown", function () {
 
-    dropdown.innerHTML = ""
+    dropdown.innerHTML = "";
     const value = this.value;
-    if (value.length >= 3) {
+    console.log(value.length)
+    if (value.length >= 2) {
         searchService.searchMovies(value).then(movies => {
-            movieList = movies.sort(sortByScore)
+            console.log(movies)
+            movieList = movies.sort(sortByScore);
             movieList.splice(0, 8).forEach(movie => {
-                const option = movieOption(movie)
-                dropdown.appendChild(option)
-                let x = document.querySelectorAll(".title")
-                // console.log(x)
-                x.forEach(element => element.addEventListener("click", (e) => {
-                    inputElement.value = e.target.textContent
-                }))
+                const option = movieOption(movie);
+                dropdown.appendChild(option);
             })
-
         })
     }
     else {
-        dropdown.innerHTML = `<p>this was deleted</p>`
+        dropdown.innerHTML = ``;
     }
 })
 
 window.addEventListener("click", (e) => {
     if (!e.target.matches(".dropdown-item")) {
-        dropdown.classList.remove("show")
+        dropdown.classList.remove("show");
+        inputElement.classList.remove("showing")
+        textholder.style.display ="none"
     }
-    
 })
 
 window.addEventListener("click", (e) => {
-    console.log(e.target)
-    if (e.target.matches(".searchTerm"))
-        dropdown.classList.add("show")
+    if (e.target.matches(".searchTerm")) {
+        dropdown.classList.add("show");
+        inputElement.classList.add("showing")
+        textholder.style.display ="block"
+    }
 })
 
 
 function movieOption(movie) {
-    let content = document.createElement("div")
-    content.classList.add("dropdown-item")
-    let movieName = document.createElement("div")
-    movieName.classList.add("title")
-    movieNameContent = document.createTextNode(`${movie.title}`)
-    movieName.appendChild(movieNameContent)
-    content.appendChild(movieName)
+    let content = document.createElement("div");
+    content.classList.add("dropdown-item");
+    let movieName = document.createElement("div");
+    movieName.classList.add("title");
+    movieNameContent = document.createTextNode(`${movie.title}`);
+    movieName.appendChild(movieNameContent);
+    content.appendChild(movieName);
 
-    let rating = document.createElement("div")
-    rating.classList.add("rating")
-    ratingContent = document.createTextNode(`${movie.vote_average}`)
-    rating.appendChild(ratingContent)
-    content.appendChild(rating)
+    let ratingYear = document.createElement("div");
+    ratingYear.classList.add("rating");
+    ratingYearContent = document.createTextNode(`${movie.vote_average} Rating, ${movie.release_date.slice(0, 4)}`);
+    ratingYear.appendChild(ratingYearContent);
+    content.appendChild(ratingYear);
 
-    let movieYear = document.createElement("div")
-    movieYear.classList.add("year")
-    movieYearContent = document.createTextNode(`${movie.release_date.slice(0, 4)}`)
-    movieYear.appendChild(movieYearContent)
-    content.appendChild(movieYear)
+    // let movieYear = document.createElement("div");
+    // movieYear.classList.add("year");
+    // movieYearContent = document.createTextNode(`${movie.release_date.slice(0, 4)}`);
+    // movieYear.appendChild(movieYearContent);
+    // content.appendChild(movieYear);
+
+    movieName.addEventListener("click", function (e) {
+        let el = this;
+        console.log(this)
+        document.querySelector(".searchTerm").value = el.textContent;
+
+    })
     return content
 }
