17b6faf518351b8ca778631eeba2bbfafbb7863d
Mantvydas <civilka.mantas@gmail.com>
2020-03-01 18:00:16 +0200

    further improve style and logic

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
 const inputElement = document.querySelector(".searchTerm");
 const buttonElement = document.querySelector(".searchButton");
 const dropdown = document.getElementById("dropdown");
-const textholder = document.querySelector(".textholder")
+const textHolder = document.querySelector(".textholder");
+
 function sortByScore(a, b) {
     return b.popularity - a.popularity
 }
@@ -14,32 +15,29 @@ class SearchApi {
     }
 
     searchMovies(queryString) {
-        let url = this._createUrl(queryString)
+        let url = this._createUrl(queryString);
         return fetch(url)
             .then((response) => response.json())
             .then((data) => {
-                let movieList = data.results
+                let movieList = data.results;
                 return movieList
             })
     }
 
     _createUrl(queryString) {
-        const sanitizedQueryString = queryString.replace(/ |&/g, function (match) { return (match == "&") ? "%26" : "%20"; })
-        const url = `${this.url}?api_key=${this.apiKey}&language=en-US&query=${sanitizedQueryString}&page=1&include_adult=false`
+        const sanitizedQueryString = queryString.replace(/ |&/g, function (match) { return (match == "&") ? "%26" : "%20"; });
+        const url = `${this.url}?api_key=${this.apiKey}&language=en-US&query=${sanitizedQueryString}&page=1&include_adult=false`;
         return url;
     }
 }
 
 let searchService = new SearchApi()
 
-inputElement.addEventListener("keydown", function () {
-
+inputElement.addEventListener("input", function (e) {
     dropdown.innerHTML = "";
-    const value = this.value;
-    console.log(value.length)
-    if (value.length >= 2) {
+    const value = e.target.value;
+    if (value.length >= 3) {
         searchService.searchMovies(value).then(movies => {
-            console.log(movies)
             movieList = movies.sort(sortByScore);
             movieList.splice(0, 8).forEach(movie => {
                 const option = movieOption(movie);
@@ -55,19 +53,18 @@ inputElement.addEventListener("keydown", function () {
 window.addEventListener("click", (e) => {
     if (!e.target.matches(".dropdown-item")) {
         dropdown.classList.remove("show");
-        inputElement.classList.remove("showing")
-        textholder.style.display ="none"
+        inputElement.classList.remove("showing");
+        textHolder.style.display = "none";
     }
 })
 
 window.addEventListener("click", (e) => {
     if (e.target.matches(".searchTerm")) {
         dropdown.classList.add("show");
-        inputElement.classList.add("showing")
-        textholder.style.display ="block"
+        inputElement.classList.add("showing");
+        textHolder.style.display = "block";
     }
 })
-
 
 function movieOption(movie) {
     let content = document.createElement("div");
@@ -84,15 +81,8 @@ function movieOption(movie) {
     ratingYear.appendChild(ratingYearContent);
     content.appendChild(ratingYear);
 
-    // let movieYear = document.createElement("div");
-    // movieYear.classList.add("year");
-    // movieYearContent = document.createTextNode(`${movie.release_date.slice(0, 4)}`);
-    // movieYear.appendChild(movieYearContent);
-    // content.appendChild(movieYear);
-
     movieName.addEventListener("click", function (e) {
         let el = this;
-        console.log(this)
         document.querySelector(".searchTerm").value = el.textContent;
 
     })
